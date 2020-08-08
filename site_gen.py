0dae8d3f6cfca1449f40a21f80e8979c98d662ef
Shria Devarakonda <54900370+shria-devarakonda@users.noreply.github.com>
2020-08-08 19:34:49 +0530

    html filename change

--- site_gen.py
+++ site_gen.py
@@ -1,6 +1,6 @@
 name = input("Enter is your name: ")
 desc = input("Enter your description: ")
 script = "<!DOCTYPE html> <html> <head> <style> h1 {text-align: center;} p {text-align: center;} </style> </head> <body style=\"background-color:blueviolet;\"> <font color = \"#FF00CB\" face = \"Comic sans MS\" size = \"7\"><br /> <h1>"
-script2 = name + "</h1> <p>" + desc + "</p> </font> </body> </html>"
+script2 = name + "</h1> <p>" + desc + " </p> </font> </body> </html>"
 with open(name + "spage.html", "w+") as f:
         f.write(script + script2)
